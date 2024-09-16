2e82c263e033309fb23d05596f007c30d5c462d7
Tausinn <tausin809@gmail.com>
2024-09-16 23:06:58 +0600

    change

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='main-container'>
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <img className='logo' src="../src/assets/logo.png" alt="logo" />
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
